59a0e3a7f5505aae0bc8ccd4fd3e47d0ddeb4ec3
BeGj <schpet1@pax.priv.no>
2025-11-19 17:24:14 +0100

    fix prerendering for demo

--- projects/demo/src/app/app.routes.server.ts
+++ projects/demo/src/app/app.routes.server.ts
@@ -2,6 +2,20 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'blog/:blogId',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return [
+        {
+          blogId: '1',
+        },
+        {
+          blogId: '2',
+        },
+      ];
+    },
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
